d3bf181ff658760124f60ff9852660ff50adda1f
yuzhongqiang <jy8yzq@163.com>
2013-07-31 20:36:10 +0800

    aaa

--- jpeg.c
+++ jpeg.c
@@ -8,86 +8,10 @@
 #include <stdlib.h>
 
 #include "types.h"
-
-typedef enum {			/* JPEG marker codes */
-  M_SOF0  = 0xc0,         /* Start Of Frame */
-  M_SOF1  = 0xc1,
-  M_SOF2  = 0xc2,
-  M_SOF3  = 0xc3,
-  
-  M_SOF5  = 0xc5,
-  M_SOF6  = 0xc6,
-  M_SOF7  = 0xc7,
-  
-  M_JPG   = 0xc8,
-  M_SOF9  = 0xc9,
-  M_SOF10 = 0xca,
-  M_SOF11 = 0xcb,
-  
-  M_SOF13 = 0xcd,
-  M_SOF14 = 0xce,
-  M_SOF15 = 0xcf,
-  
-  M_DHT   = 0xc4,
-  
-  M_DAC   = 0xcc,
-  
-  M_RST0  = 0xd0,
-  M_RST1  = 0xd1,
-  M_RST2  = 0xd2,
-  M_RST3  = 0xd3,
-  M_RST4  = 0xd4,
-  M_RST5  = 0xd5,
-  M_RST6  = 0xd6,
-  M_RST7  = 0xd7,
-  
-  M_SOI   = 0xd8,    /* Start Of Image */
-  M_EOI   = 0xd9,    /* End Of Image */
-  M_SOS   = 0xda,   /* Start Of Scan */
-  M_DQT   = 0xdb,
-  M_DNL   = 0xdc,
-  M_DRI   = 0xdd,
-  M_DHP   = 0xde,
-  M_EXP   = 0xdf,
-  
-  M_APP0  = 0xe0,
-  M_APP1  = 0xe1,
-  M_APP2  = 0xe2,
-  M_APP3  = 0xe3,
-  M_APP4  = 0xe4,
-  M_APP5  = 0xe5,
-  M_APP6  = 0xe6,
-  M_APP7  = 0xe7,
-  M_APP8  = 0xe8,
-  M_APP9  = 0xe9,
-  M_APP10 = 0xea,
-  M_APP11 = 0xeb,
-  M_APP12 = 0xec,
-  M_APP13 = 0xed,
-  M_APP14 = 0xee,
-  M_APP15 = 0xef,
-  
-  M_JPG0  = 0xf0,
-  M_JPG13 = 0xfd,
-  M_COM   = 0xfe,
-  
-  M_TEM   = 0x01,
-  
-  M_ERROR = 0x100
-} JPEG_MARKER;
+#include "jpeg.h"
 
 
-
-
-
-
-
-
-
-
-
-
-image_t* ipegLoadImage(const char* filename, int iscolor)
+image_t* jpegLoadImage(const char* filename, int iscolor)
 {
 	int i, ret = 0;
 	mat_t *mat = NULL;
@@ -100,10 +24,77 @@ image_t* ipegLoadImage(const char* filename, int iscolor)
 	if (NULL == fp)
 	return NULL;
 
-	/* read HEAD*/
-	
+	/* read HEAD: FFD8*/
+	fread(fp, sizeof(char), 2, buf);
 
+	/* continue reading */
+	fread(fp, sizeof(char), 2, buf);
 
+	switch (buf[1]) {
+case M_SOF0	:
+case M_SOF1	:
+case M_SOF2	:
+case M_SOF3	:
+
+case M_SOF5	:
+case M_SOF6	:
+case M_SOF7	:
+
+case M_JPG	:
+case M_SOF9	:
+case M_SOF10 :
+case M_SOF11 :
+
+case M_SOF13 :
+case M_SOF14 :
+case M_SOF15 :
+
+case M_DHT	:
+
+case M_DAC	:
+
+case M_RST0	:
+case M_RST1	:
+case M_RST2	:
+case M_RST3	:
+case M_RST4	:
+case M_RST5	:
+case M_RST6	:
+case M_RST7	:
+
+case M_SOI	:
+case M_EOI	:
+case M_SOS	:
+case M_DQT	:
+case M_DNL	:
+case M_DRI	:
+case M_DHP	:
+case M_EXP	:
+
+case M_APP0	:
+case M_APP1	:
+case M_APP2	:
+case M_APP3	:
+case M_APP4	:
+case M_APP5	:
+case M_APP6	:
+case M_APP7	:
+case M_APP8	:
+case M_APP9	:
+case M_APP10 :
+case M_APP11 :
+case M_APP12 :
+case M_APP13 :
+case M_APP14 :
+case M_APP15 :
+
+case M_JPG0	:
+case M_JPG13 :
+case M_COM	:
+
+case M_TEM	:
+
+		}
 
 
     
