d867ce3e8eeb9fb7b57bfae092bfc2c5dad8c09a
yuzhongqiang <jy8yzq@163.com>
2013-08-16 15:19:04 +0800

    add jpeg-doc.txt
    Signed-off-by: yuzhongqiang <jy8yzq@163.com>

--- jpeg.c
+++ jpeg.c
@@ -10,11 +10,13 @@
 #include "types.h"
 #include "jpeg.h"
 
+#define DBG(x) (x)
 
 image_t* jpegLoadImage(const char* filename, int iscolor)
 {
 	int i, ret = 0;
 	mat_t *mat = NULL;
+	int len = 0;
 	FILE *fp;
 	unsigned char buf[1024];
 
@@ -24,81 +26,76 @@ image_t* jpegLoadImage(const char* filename, int iscolor)
 	if (NULL == fp)
 	return NULL;
 
-	/* read HEAD: FFD8*/
+	/* read JPEG HEADER: FFD8*/
 	fread(fp, sizeof(char), 2, buf);
+	if (buf[0] != 0xFF || buf[1] != 0xD8)
+		goto err1;
 
-	/* continue reading */
-	fread(fp, sizeof(char), 2, buf);
+#define GET_SEG_LEN \	
+	fread(fp, sizeof(char), 2, buf); \
+	len = buf[0] << 8 | buf[1]; \
+	DBG(printf("segment length = %d\n", len))
 
-	switch (buf[1]) {
-case M_SOF0	:
-case M_SOF1	:
-case M_SOF2	:
-case M_SOF3	:
+	while (1) {
+		/* continue reading */
+		fread(fp, sizeof(char), 2, buf);
+		if (buf[0] != 0xFF)
+			goto err1;
 
-case M_SOF5	:
-case M_SOF6	:
-case M_SOF7	:
+		switch (buf[1]) {
+		case M_SOF0	:
+		case M_SOF1	:
 
-case M_JPG	:
-case M_SOF9	:
-case M_SOF10 :
-case M_SOF11 :
 
-case M_SOF13 :
-case M_SOF14 :
-case M_SOF15 :
+		case M_JPG	:
+		case M_SOF9	:
+		case M_SOF10 :
+		case M_SOF11 :
 
-case M_DHT	:
+		case M_SOF13 :
+		case M_SOF14 :
+		case M_SOF15 :
 
-case M_DAC	:
+		case M_DHT	:
 
-case M_RST0	:
-case M_RST1	:
-case M_RST2	:
-case M_RST3	:
-case M_RST4	:
-case M_RST5	:
-case M_RST6	:
-case M_RST7	:
+		case M_DAC	:
 
-case M_SOI	:
-case M_EOI	:
-case M_SOS	:
-case M_DQT	:
-case M_DNL	:
-case M_DRI	:
-case M_DHP	:
-case M_EXP	:
+		case M_RST0	:
+		case M_RST1	:
 
-case M_APP0	:
-case M_APP1	:
-case M_APP2	:
-case M_APP3	:
-case M_APP4	:
-case M_APP5	:
-case M_APP6	:
-case M_APP7	:
-case M_APP8	:
-case M_APP9	:
-case M_APP10 :
-case M_APP11 :
-case M_APP12 :
-case M_APP13 :
-case M_APP14 :
-case M_APP15 :
+		//case M_SOI	:
+		case M_EOI	:  // end of image
+			break;
+			
+		case M_SOS	:
+		case M_DQT	:
+		case M_DNL	:
+		case M_DRI	:
+		case M_DHP	:
+		case M_EXP	:
 
-case M_JPG0	:
-case M_JPG13 :
-case M_COM	:
+		case M_APP0	:			
+			GET_SEG_LEN;
+			
+			break;
+			
+		case M_APP1	:
 
-case M_TEM	:
+		case M_JPG0	:
+		case M_JPG13 :
+		case M_COM	:
+
+		case M_TEM	:
 
 		}
-
+	}
 
     
-    return image;    
+    return image;  
+
+err1:
+	return NULL;
+	
 }
 
 
